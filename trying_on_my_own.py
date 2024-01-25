a51d79a1a9822371ce9c6518462ab659d71e5a9b
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-26 02:35:26 +0800

    commit ko muna to kasi nagiisip pa ako ano ang magiging flow or conidtion para makuha nag 6000

--- trying_on_my_own.py
+++ trying_on_my_own.py
@@ -2,3 +2,12 @@
 # searching it to the internet or chatgpt.
 
 # Note: I watch the tutorial first in YT about how to use the specific function and apply it here in my task
+
+income = 45000
+payable_tax = 0 
+print("Given income is ", income)
+
+print("\n")
+
+if income <= 10000:
+    payable_tax = 0
